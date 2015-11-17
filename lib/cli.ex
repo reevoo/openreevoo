ec7c38faeb74acb248fd632143ffe062622eba97
Richard Batty <richard.batty@reevoo.com>
2015-11-17 16:36:22 +0000

    Put common prompt functionality into one function

--- lib/cli.ex
+++ lib/cli.ex
@@ -2,6 +2,10 @@ defmodule Openreevoo.Cli do
   @moduledoc """
   The command line interface that users leave their review with
   """
+  @library_prompt "Which library would you like to review? (choose a number):"
+  @rating_prompt "Out of 10, how would you rate"
+  @good_points_prompt "What was good about"
+  @bad_points_prompt "What was bad about"
 
   alias Openreevoo.Review
 
@@ -11,16 +15,16 @@ defmodule Openreevoo.Cli do
 
   defp choose_library do
     IO.puts Openreevoo.Dependencies.formatted
-    library_index_str = IO.gets "Which library would you like to review? (choose a number): "
+    library_index_str = prompt(@library_prompt, "")
     {library_index, _} = library_index_str |> Integer.parse
     Enum.at(Openreevoo.Dependencies.list, library_index)
   end
 
   defp get_review(library) do
     %Review{
-      rating: rating(library),
-      good_points: good_points(library),
-      bad_points: bad_points(library)
+      rating: prompt(@rating_prompt, library),
+      good_points: prompt(@good_points_prompt, library),
+      bad_points: prompt(@bad_points_prompt, library)
     }
   end
 
@@ -28,15 +32,7 @@ defmodule Openreevoo.Cli do
     "Rating: #{review.rating} Good points: #{review.good_points} Bad points: #{review.bad_points}"
   end
 
-  defp rating(library) do
-    IO.gets "Out of 10, how would you rate #{library}?\n> "
-  end
-
-  defp good_points(library) do
-    IO.gets "What was good about #{library}?\n> "
-  end
-
-  defp bad_points(library) do
-    IO.gets "What was bad about #{library}?\n> "
+  defp prompt(question, library) do
+    IO.gets "#{question} #{library}?\n> "
   end
 end
